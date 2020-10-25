7d36ea4946583894459f85aac64be09205b1a123
DanielVrublevskyi <daniil24267@gmail.com>
2020-10-25 21:02:11 +0200

    LOg

--- src/test/java/com/ilcarro/qa/tests/TestBase.java
+++ src/test/java/com/ilcarro/qa/tests/TestBase.java
@@ -2,17 +2,30 @@ package com.ilcarro.qa.tests;
 
 import com.ilcarro.qa.framework.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
+
+import java.lang.reflect.Method;
 
 public class TestBase {
 
     protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
     @BeforeSuite
     public void setUp(){
         app.init();
 
-
+    }
+    @BeforeMethod
+    public  void  startTest(Method m){
+        logger.info("Start test "+ m.getName());
+    }
+    @AfterMethod
+    public  void  stopTest(Method m){
+        logger.info("Stop test " + m.getName());
+        logger.info("===============================================================================================");
     }
 
 
